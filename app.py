784622a152846992f7ea519788286118f9e6c485
antarikshshekhawat <antariksh.behror@gmail.com>
2026-04-17 16:56:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -297,9 +297,7 @@ def health():
         "cache": disk_meta,
     })
 
+import os
 
 if __name__ == "__main__":
-    _trigger_refresh_if_needed()
-   import os
-
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
